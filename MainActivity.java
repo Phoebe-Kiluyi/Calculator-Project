12162410fd898ce74a81bdce5b8b1f6271ba480d
Phoebe Kiluyi <134553522+Phoebe-Kiluyi@users.noreply.github.com>
2023-08-09 14:09:19 +0300

    Update MainActivity.java
    Android Studio Project

--- MainActivity.java
+++ MainActivity.java
@@ -1,263 +1,212 @@
-package com.example.calcassignment;
+package com.example.bscscalcassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
-
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-    public class MainActivity extends AppCompatActivity {
-        EditText editTextNumber;
-        EditText editTextNumber2;
-        TextView textViewResult;
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            // Find the views by their IDs
-            editTextNumber = findViewById(R.id.editTextNumber);
-            editTextNumber2 = findViewById(R.id.editTextNumber2);
-            textViewResult = findViewById(R.id.editTextNumber5);
-            // Find button1 and set click listener
-            Button button1 = findViewById(R.id.button1);
-            button1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Handle button1 click here
-                    onButton1Click();
-                }
-            });
+public class MainActivity extends AppCompatActivity {
 
-            // Find button2 and set click listener
-            Button button2 = findViewById(R.id.button2);
-            button2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Handle button2 click here
-                    onButton2Click();
-                }
-            });
-            // Find button3 and set click listener
-            Button button3 = findViewById(R.id.button3);
-            button3.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Handle button2 click here
-                    onButton3Click();
-                }
-            });
-            // Find button4 and set click listener
-            Button button4 = findViewById(R.id.button4);
-            button4.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Handle button2 click here
-                    onButton4Click();
-                }
-            });
-            // Find button5 and set click listener
-            Button button5 = findViewById(R.id.button5);
-            button5.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Handle button5 click here
-                    onButton5Click();
-                }
-            });
-            // Find button6 and set click listener
-            Button button6 = findViewById(R.id.button6);
-            button6.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Handle button6 click here
-                    onButton6Click();
-                }
-            });
-            // Find button7 and set click listener
-            Button button7 = findViewById(R.id.button7);
-            button7.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Handle button7 click here
-                    onButton7Click();
-                }
-            });
-            // Find button8 and set click listener
-            Button button8 = findViewById(R.id.button8);
-            button8.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Handle button8 click here
-                    onButton8Click();
-                }
-            });
-            // Find button9 and set click listener
-            Button button9 = findViewById(R.id.button9);
-            button9.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Handle button9 click here
-                    onButton9Click();
-                }
-            });
-            // Find "MULTI" button and set click listener
-            Button buttonMULTI = findViewById(R.id.buttonMulti);
-            buttonMULTI.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Handle "MULTI" button click here
-                    performMultiplication();
-                }
-            });
-            // Find "ADD" button and set click listener
-            Button buttonADD = findViewById(R.id.buttonAdd);
-            buttonADD.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Handle "ADD" button click here
-                    performAddition();
-                }
-            });
-            // Find "SUBTRACT" button and set click listener
-            Button buttonSUB = findViewById(R.id.buttonSub);
-            buttonSUB.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Handle "SUB" button click here
-                    performSubtraction();
-                }
-            });
-            // Find "DIV" button and set click listener
-            Button buttonDIV = findViewById(R.id.buttonDiv);
-            buttonDIV.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Handle "DIV" button click here
-                    performDivision();
-                }
-            });
+    EditText firstNumberEditText, secondNumberEditText, operatorEditText, AnswerEditText;
+    Button add, sub, multi, div, refresh, equals;
 
-        }
 
-        private void onButton1Click() {
-            // Your button click handling code goes here
-            Toast.makeText(this, "Button 1 clicked!", Toast.LENGTH_SHORT).show();
-        }
+    private String selectedOperator = ""; // To store the selected operator
+    private boolean enteringSecondNumber = false;
 
-        private void onButton2Click() {
-            // Your button2 click handling code goes here
-            // For example, you can display a toast message when button2 is clicked
-            Toast.makeText(this, "Button 2 clicked!", Toast.LENGTH_SHORT).show();
-        }
-        private void onButton3Click() {
-            // Your button2 click handling code goes here
-            // For example, you can display a toast message when button3 is clicked
-            Toast.makeText(this, "Button 3 clicked!", Toast.LENGTH_SHORT).show();
-        }
-        private void onButton4Click() {
-            // Your button4 click handling code goes here
-            // For example, you can display a toast message when button4 is clicked
-            Toast.makeText(this, "Button 4 clicked!", Toast.LENGTH_SHORT).show();
-        }
-        private void onButton5Click() {
-            // Your button5 click handling code goes here
-            // For example, you can display a toast message when button5 is clicked
-            Toast.makeText(this, "Button 5 clicked!", Toast.LENGTH_SHORT).show();
-        }
-        private void onButton6Click() {
-            // Your button6 click handling code goes here
-            // For example, you can display a toast message when button6 is clicked
-            Toast.makeText(this, "Button 6 clicked!", Toast.LENGTH_SHORT).show();
-        }
-        private void onButton7Click() {
-            // Your button7 click handling code goes here
-            // For example, you can display a toast message when button7 is clicked
-            Toast.makeText(this, "Button 7 clicked!", Toast.LENGTH_SHORT).show();
-        }
-        private void onButton8Click() {
-            // Your button8 click handling code goes here
-            // For example, you can display a toast message when button8 is clicked
-            Toast.makeText(this, "Button 8 clicked!", Toast.LENGTH_SHORT).show();
-        }
-        private void onButton9Click() {
-            // Your button9 click handling code goes here
-            // For example, you can display a toast message when button9 is clicked
-            Toast.makeText(this, "Button 9 clicked!", Toast.LENGTH_SHORT).show();
-        }
-        // Method to handle multiplication functionality
-        private void performMultiplication() {
-            // Get the values from the EditText fields, convert them to numbers
-            // Perform the multiplication operation
-            // Update the result EditText with the multiplication result
-            // For example:
-            try {
-                int num1 = Integer.parseInt(editTextNumber.getText().toString());
-                int num2 = Integer.parseInt(editTextNumber2.getText().toString());
-                int result = num1 * num2;
-                textViewResult = findViewById(R.id.editTextNumber5);
-                        textViewResult.setText(String.valueOf(result));
-            } catch (NumberFormatException e) {
-                // Handle the case when the user inputs invalid numbers
-                Toast.makeText(this, "Please enter valid numbers.", Toast.LENGTH_SHORT).show();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firstNumberEditText = findViewById(R.id.editTextNumber1);
+        secondNumberEditText = findViewById(R.id.editTextNumber2);
+        operatorEditText = findViewById(R.id.editTextNumber3);
+        AnswerEditText = findViewById(R.id.textViewResult);
+
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+        Button button7 = findViewById(R.id.button7);
+        Button button8 = findViewById(R.id.button8);
+        Button button9 = findViewById(R.id.button9);
+        Button button0 = findViewById(R.id.button0);
+
+        add = findViewById(R.id.buttonAdd);
+        sub = findViewById(R.id.buttonSub);
+        multi = findViewById(R.id.buttonMulti);
+        div = findViewById(R.id.buttonDiv);
+        refresh = findViewById(R.id.buttonRefresh);
+        equals = findViewById(R.id.buttonEquals);
+
+        firstNumberEditText.setInputType(InputType.TYPE_NULL);
+        secondNumberEditText.setInputType(InputType.TYPE_NULL);
+        operatorEditText.setInputType(InputType.TYPE_NULL);
+        AnswerEditText.setInputType(InputType.TYPE_NULL);
+
+        // Set click listeners for number buttons
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberClicked("1");
             }
-        }
-        // Method to handle addition functionality
-        private void performAddition() {
-            // Get the values from the EditText fields, convert them to numbers
-            // Perform the addition operation
-            // Update the result EditText with the addition result
-            // For example:
-            try {
-                int num1 = Integer.parseInt(editTextNumber.getText().toString());
-                int num2 = Integer.parseInt(editTextNumber2.getText().toString());
-                int result = num1 + num2;
-                textViewResult = findViewById(R.id.editTextNumber5);
-                textViewResult.setText(String.valueOf(result));
-            } catch (NumberFormatException e) {
-                // Handle the case when the user inputs invalid numbers
-                Toast.makeText(this, "Please enter valid numbers.", Toast.LENGTH_SHORT).show();
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberClicked("2");
             }
-        }
-        // Method to handle subtraction functionality
-        private void performSubtraction() {
-            // Get the values from the EditText fields, convert them to numbers
-            // Perform the subtraction operation
-            // Update the result EditText with the subtraction result
-            // For example:
-            try {
-                int num1 = Integer.parseInt(editTextNumber.getText().toString());
-                int num2 = Integer.parseInt(editTextNumber2.getText().toString());
-                int result = num1 - num2;
-                textViewResult.setText(String.valueOf(result));
-            } catch (NumberFormatException e) {
-                // Handle the case when the user inputs invalid numbers
-                Toast.makeText(this, "Please enter valid numbers.", Toast.LENGTH_SHORT).show();
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberClicked("3");
             }
-        }
-        // Method to handle division functionality
-        private void performDivision() {
-            // Get the values from the EditText fields, convert them to numbers
-            // Perform the division operation
-            // Update the result EditText with the division result
-            // For example:
-            try {
-                int num1 = Integer.parseInt(editTextNumber.getText().toString());
-                int num2 = Integer.parseInt(editTextNumber2.getText().toString());
-                if (num2 == 0) {
-                    // Handle division by zero case
-                    Toast.makeText(this, "Cannot divide by zero.", Toast.LENGTH_SHORT).show();
-                } else {
-                    double result = (double) num1 / num2;
-                    textViewResult = findViewById(R.id.editTextNumber5);
-                    textViewResult.setText(String.valueOf(result));
-                }
-            } catch (NumberFormatException e) {
-                // Handle the case when the user inputs invalid numbers
-                Toast.makeText(this, "Please enter valid numbers.", Toast.LENGTH_SHORT).show();
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberClicked("4");
             }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberClicked("5");
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberClicked("6");
+            }
+        });
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberClicked("7");
+            }
+        });
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberClicked("8");
+            }
+        });
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberClicked("9");
+            }
+        });
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNumberClicked("0");
+            }
+        });
+
+
+        // Set click listeners for operator buttons
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onOperatorButtonClicked("+");
+            }
+        });
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onOperatorButtonClicked("-");
+            }
+        });
+        div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onOperatorButtonClicked("/");
+            }
+        });
+        multi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onOperatorButtonClicked("*");
+            }
+        });
+
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firstNumberEditText.setText("");
+                secondNumberEditText.setText("");
+                operatorEditText.setText("");
+                AnswerEditText.setText("");
+                selectedOperator = "";
+            }
+        });
+        equals.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View v){
+                onEqualsButtonClicked();
+            }
+        });
+    }
+
+    private void onNumberClicked(String number) {
+        if (!enteringSecondNumber) {
+            String currentText = firstNumberEditText.getText().toString();
+            firstNumberEditText.setText(currentText + number);
+        } else {
+            String currentText = secondNumberEditText.getText().toString();
+            secondNumberEditText.setText(currentText + number);
         }
+    }
+
+    private void onOperatorButtonClicked(String operator) {
+        // If the operator is not selected yet, set the selectedOperator
+        // and update the operatorEditText to show the selected operator
+        if (!enteringSecondNumber) {
+            selectedOperator = operator;
+            operatorEditText.setText(selectedOperator);
+            enteringSecondNumber = true;
         }
-    
+    }
+
+    private void onEqualsButtonClicked() {
+        if (!firstNumberEditText.getText().toString().isEmpty() &&
+                !secondNumberEditText.getText().toString().isEmpty() &&
+                !selectedOperator.isEmpty()) {
+            double first = Double.parseDouble(firstNumberEditText.getText().toString());
+            double second = Double.parseDouble(secondNumberEditText.getText().toString());
+            double result = 0;
+
+            switch (selectedOperator) {
+                case "+":
+                    result = first + second;
+                    break;
+                case "-":
+                    result = first - second;
+                    break;
+                case "*":
+                    result = first * second;
+                    break;
+                case "/":
+                    if (second != 0) {
+                        result = first / second;
+                    } else {
+                        AnswerEditText.setText("Cannot divide by zero");
+                        return;
+                    }
+                    break;
+            }
+
+            AnswerEditText.setText(String.valueOf(result));
+            enteringSecondNumber = false;
+        }
+    }
+}
